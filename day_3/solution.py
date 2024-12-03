28cf81aadc4883bf6038f49226218a5f5f04c371
daniel.cahall <danielenricocahall@gmail.com>
2024-12-03 08:45:56 -0500

    part 2 done, cleanup

--- day_3/solution.py
+++ day_3/solution.py
@@ -2,15 +2,46 @@ import operator
 import re
 from functools import reduce
 
+MUL_PATTERN = re.compile(r'mul\(([0-9]+,[0-9]+)\)')
+DONT_PATTERN = re.compile(r"don't\(\)")
+DO_PATTERN = re.compile(r"do\(\)")
+
+
+def part_1(corrupted_memory: str) -> int:
+    matches = MUL_PATTERN.findall(corrupted_memory)
+    result = 0
+    for match in matches:
+        result += multiply_instructions(match)
+    return result
+
+
+def part_2(corrupted_memory: str) -> int:
+    current_string = ""
+    i = 0
+    result = 0
+    do = True
+    while i < len(corrupted_memory):
+        current_string += corrupted_memory[i]
+        if match := MUL_PATTERN.findall(current_string):
+            if do:
+                result += multiply_instructions(match[0])
+            current_string = ""
+        elif DONT_PATTERN.findall(current_string):
+            do = False
+        elif DO_PATTERN.findall(current_string):
+            do = True
+        i += 1
+    return result
+
+
+def multiply_instructions(instructions: str) -> int:
+    operands = map(int, instructions.split(","))
+    return reduce(operator.mul, operands)
+
+
 if __name__ == "__main__":
     with open('./puzzle.txt') as fp:
-        instruction = fp.read()
-        matches = re.findall(r'mul\(([0-9]+,[0-9]+)\)', instruction)
-        result = 0
-        for match in matches:
-            operands = map(int, match.split(","))
-            result += reduce(operator.mul, operands)
+        corrupted_memory = fp.read()
 
-        print(result)
-
-
+        print(part_1(corrupted_memory))
+        print(part_2(corrupted_memory))
