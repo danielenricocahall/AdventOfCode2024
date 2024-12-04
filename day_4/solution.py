357feb7befc3db3daf8aebf0c52bc5e376119bd7
daniel.cahall <danielenricocahall@gmail.com>
2024-12-04 09:12:14 -0500

    simplify

--- day_4/solution.py
+++ day_4/solution.py
@@ -8,10 +8,10 @@ def find_occurrences_horizontally(puzzle: list[str]) -> int:
     return xmas_occurrences_horizontally
 
 
-def find_occurrences_diagonally(puzzle: list[str], strings: list[str]) -> int:
+def find_occurrences_diagonally(puzzle: list[str]) -> int:
     diagonal_occurrences = 0
     cols = len(puzzle[0])
-    for s in strings:
+    for s in ("XMAS", "SAMX"):
         for i in range(len(puzzle) - len(s) + 1):
             for j in range(cols - len(s) + 1):
                 occurrence = all(puzzle[i + k][j + k] == c for k, c in enumerate(s))
@@ -27,14 +27,12 @@ def display_puzzle(puzzle: list[str]):
     print(*puzzle, sep=f"\n")
 
 if __name__ == "__main__":
-    with open('./puzzle_test.txt') as fp:
+    with open('./puzzle.txt') as fp:
         puzzle = list(map(str.strip, fp.readlines()))
         rows = len(puzzle)
         cols = len(puzzle[0])
         horizontal_occurrences = find_occurrences_horizontally(puzzle)
         vertical_occurrences = find_occurrences_horizontally(transpose_puzzle(puzzle))
-        diagonal_occurrences = find_occurrences_diagonally(puzzle, ["XMAS", "SAMX"])
-        other_diagonal_occurrences = find_occurrences_diagonally(puzzle[::-1], ["XMAS", "SAMX"])
+        diagonal_occurrences = find_occurrences_diagonally(puzzle)
+        other_diagonal_occurrences = find_occurrences_diagonally(puzzle[::-1])
         print(diagonal_occurrences + horizontal_occurrences + vertical_occurrences + other_diagonal_occurrences)
-
-        print(find_occurrences_diagonally(puzzle, ["SAM", "MAS"]) + find_occurrences_diagonally(puzzle[::-1], ["MAS", "SAM"]))
