2df4df4c58da0fb3798524cbb6dbb1ec8e8f1f35
daniel.cahall <danielenricocahall@gmail.com>
2024-12-06 07:27:49 -0500

    brute force, extremely slow for part 2

--- day_6/solution.py
+++ day_6/solution.py
@@ -1,35 +1,44 @@
-from collections import namedtuple
+import copy
+from collections import namedtuple, defaultdict
 
 GUARD_SYMBOL = "^"
-OBSTACLE_SYMBOL = "#"
+OBSTRUCTION_SYMBOL = "#"
 ROTATION_DEGREES = 90
 
-GuardLocation = namedtuple('GuardLocation', ['x', 'y'])
+MapLocation = namedtuple('MapLocation', ['x', 'y'])
 
 
-def find_initial_guard_location(puzzle: list[list[str]]) -> GuardLocation:
+def find_initial_guard_location(puzzle: list[list[str]]) -> MapLocation:
     for y, row in enumerate(puzzle):
         if GUARD_SYMBOL in row:
             x = row.index(GUARD_SYMBOL)
-            return GuardLocation(x, y)
+            return MapLocation(x, y)
 
 
-def predict_guard_route(puzzle: list[list[str]], guard_location: GuardLocation):
+def predict_guard_route(puzzle: list[list[str]],
+                        guard_location: MapLocation,
+                        track_obstructions: bool = False,
+                        experimental_obstruction_location: MapLocation | None = None):
     distinct_positions = set()
     current_rotation = 0
+    obstructions = defaultdict(int)
     rotation_to_next_location = {
-        ROTATION_DEGREES * 0: lambda guard_location: GuardLocation(guard_location.x, guard_location.y - 1),
-        ROTATION_DEGREES * 1: lambda guard_location: GuardLocation(guard_location.x + 1, guard_location.y),
-        ROTATION_DEGREES * 2: lambda guard_location: GuardLocation(guard_location.x, guard_location.y + 1),
-        ROTATION_DEGREES * 3: lambda guard_location: GuardLocation(guard_location.x - 1, guard_location.y)
+        ROTATION_DEGREES * 0: lambda guard_location: MapLocation(guard_location.x, guard_location.y - 1),
+        ROTATION_DEGREES * 1: lambda guard_location: MapLocation(guard_location.x + 1, guard_location.y),
+        ROTATION_DEGREES * 2: lambda guard_location: MapLocation(guard_location.x, guard_location.y + 1),
+        ROTATION_DEGREES * 3: lambda guard_location: MapLocation(guard_location.x - 1, guard_location.y)
     }
     while not guard_leaving_map(puzzle, guard_location):
-        next_guard_location = rotation_to_next_location[current_rotation](guard_location)
-        if puzzle[next_guard_location.y][next_guard_location.x] != OBSTACLE_SYMBOL:
-            guard_location = next_guard_location
+        next_potential_guard_location = rotation_to_next_location[current_rotation](guard_location)
+        if puzzle[next_potential_guard_location.y][next_potential_guard_location.x] != OBSTRUCTION_SYMBOL:
+            guard_location = next_potential_guard_location
             distinct_positions.add(guard_location)
         else:
             current_rotation = (current_rotation + ROTATION_DEGREES) % (4 * ROTATION_DEGREES)
+            if track_obstructions:
+                obstructions[(current_rotation, next_potential_guard_location)] += 1
+                if obstructions[(current_rotation, experimental_obstruction_location)] > 1:
+                    return -1
         # for debugging on the test
         # print(f"***STEP {len(distinct_positions)} TAKEN***")
         # puzzle[guard_location.y][guard_location.x] = "X"
@@ -37,10 +46,22 @@ def predict_guard_route(puzzle: list[list[str]], guard_location: GuardLocation):
     return len(distinct_positions)
 
 
-
-def guard_leaving_map(puzzle: list[list[str]], guard_location: GuardLocation) -> bool:
+def guard_leaving_map(puzzle: list[list[str]], guard_location: MapLocation) -> bool:
     x, y = guard_location
     return y >= len(puzzle) - 1 or x >= len(puzzle[0]) - 1 or y < 0 or x < 0
+
+
+def find_obstruction_locations_to_cause_loops(puzzle: list[list[str]], guard_location: MapLocation):
+    number_of_obstruction_locations = 0
+    for y in range(len(puzzle)):
+        for x in range(len(puzzle[0])):
+            if puzzle[y][x] != OBSTRUCTION_SYMBOL and MapLocation(y, x) != guard_location:
+                test_puzzle = copy.deepcopy(puzzle)
+                test_puzzle[y][x] = OBSTRUCTION_SYMBOL
+                obstruction_location = MapLocation(x, y)
+                if predict_guard_route(test_puzzle, guard_location, True, obstruction_location) == -1:
+                    number_of_obstruction_locations += 1
+    return number_of_obstruction_locations
 
 
 def display_puzzle(puzzle: list[str]):
@@ -53,3 +74,4 @@ if __name__ == "__main__":
         puzzle = [list(line[0]) for line in lines]
         guard_location = find_initial_guard_location(puzzle)
         print(predict_guard_route(puzzle, guard_location))
+        print(find_obstruction_locations_to_cause_loops(puzzle, guard_location))
