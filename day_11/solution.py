4c807576a4124583bd35eaeb98b12844ed9d2775
daniel.cahall <danielenricocahall@gmail.com>
2024-12-11 18:41:40 -0500

    working for part 2

--- day_11/solution.py
+++ day_11/solution.py
@@ -1,20 +1,42 @@
+from collections import Counter, defaultdict
+from datetime import datetime
+from functools import lru_cache
 
-def blink(stones: list[int], num_blinks: int) -> list[int]:
-    for _ in range(num_blinks):
-        stones = apply_rules(stones)
-    return stones
 
-def apply_rules(stones: list[int]) -> list[int]:
-    new_stones = []
+def unpack_dict(vals: dict[int | tuple[int, int], int]):
+    unpacked = defaultdict(int)
+    for k, v in vals.items():
+        if isinstance(k, int):
+            unpacked[k] += v
+        elif isinstance(k, tuple):
+            for val in k:
+                unpacked[val] += v
+    return unpacked
+
+
+def blink(stones: list[int], num_blinks: int) -> int:
+    count = len(stones)
+    foo = defaultdict(int)
     for stone in stones:
-        if stone == 0:
-            new_stones.append(1)
-        elif len(str(stone)) % 2 == 0:
-            splitted_stones = split_stone(stone)
-            new_stones.extend(splitted_stones)
-        else:
-            new_stones.append(stone * 2024)
-    return new_stones
+        foo[stone] += 1
+    for _ in range(num_blinks):
+        bar = defaultdict(int)
+        for k, v in foo.items():
+            bar[apply_rule(k)] += v
+        count += sum(v for k, v in bar.items() if isinstance(k, tuple))
+        bar = unpack_dict(bar)
+        foo = bar
+    return count
+
+
+def apply_rule(stone: int) -> int | tuple[int, int]:
+    if stone == 0:
+        return 1
+    elif len(str(stone)) % 2 == 0:
+        splitted_stones = split_stone(stone)
+        return splitted_stones
+    else:
+        return stone * 2024
 
 
 def split_stone(stone: int) -> tuple[int, int]:
@@ -33,6 +55,8 @@ def strip_leading_zeroes_if_needed(stone_str: str):
 if __name__ == "__main__":
     with open('./puzzle.txt') as fp:
         stones = list(map(int, fp.read().split()))
-    stones_after_blinking = blink(stones, 25)
-    print(len(stones_after_blinking))
-
+    start = datetime.now()
+    stones_after_blinking = blink(stones, 75)
+    end = datetime.now()
+    print(end - start)
+    print(stones_after_blinking)
